02744bed65d684cc3f0e8099bd977ef9646fed05
apajkan <aleksandar.pajkanovic@etf.unibl.org>
2020-11-27 16:22:00 +0100

    RGB tests passing, no sync

---
+++ src/main/scala/mm2hdmi/mm2hdmi.scala
@@ -0,0 +1,138 @@
+package mm2hdmi
+
+import chisel3._
+
+/**
+  * Interface a memory section to HDMI
+  */
+class MM2hdmi extends Module {
+  val io = IO(new Bundle {
+    val data          = Input(UInt(16.W))
+    val address       = Input(UInt(16.W))
+    val newData       = Input(UInt(1.W))
+    val red           = Output(UInt(8.W))
+    val blue          = Output(UInt(8.W))
+    val green         = Output(UInt(8.W))
+    val vSync         = Output(UInt(1.W))
+    val hSync         = Output(UInt(1.W))
+  })
+
+  var j = 0
+  var i = 0
+  var k = 16384
+
+  val tempNew = RegInit(0.U(1.W))
+
+
+  val regNewData = RegNext(io.newData)
+
+  val regHSync = RegInit(0.U(1.W))
+  regHSync := regHSync + 1.U
+  io.hSync := regHSync
+
+  val regAddress = RegNext(io.address)
+  val regData = RegNext(io.data)
+
+  val regMuxRed0 = RegNext(Mux(regData(0) === 0.U, 0.U, 255.U))
+  val regMuxRed1 = RegNext(Mux(regData(1) === 0.U, 0.U, 255.U))
+  val regMuxRed2 = RegNext(Mux(regData(2) === 0.U, 0.U, 255.U))
+  val regMuxRed3 = RegNext(Mux(regData(3) === 0.U, 0.U, 255.U))
+  val regMuxRed4 = RegNext(Mux(regData(4) === 0.U, 0.U, 255.U))
+  val regMuxRed5 = RegNext(Mux(regData(5) === 0.U, 0.U, 255.U))
+  val regMuxRed6 = RegNext(Mux(regData(6) === 0.U, 0.U, 255.U))
+  val regMuxRed7 = RegNext(Mux(regData(7) === 0.U, 0.U, 255.U))
+  val regMuxRed8 = RegNext(Mux(regData(8) === 0.U, 0.U, 255.U))
+  val regMuxRed9 = RegNext(Mux(regData(9) === 0.U, 0.U, 255.U))
+  val regMuxRed10 = RegNext(Mux(regData(10) === 0.U, 0.U, 255.U))
+  val regMuxRed11 = RegNext(Mux(regData(11) === 0.U, 0.U, 255.U))
+  val regMuxRed12 = RegNext(Mux(regData(12) === 0.U, 0.U, 255.U))
+  val regMuxRed13 = RegNext(Mux(regData(13) === 0.U, 0.U, 255.U))
+  val regMuxRed14 = RegNext(Mux(regData(14) === 0.U, 0.U, 255.U))
+  val regMuxRed15 = RegNext(Mux(regData(15) === 0.U, 0.U, 255.U))
+
+  val outControlCounter = RegInit(0.U(4.W))
+  outControlCounter := Mux(outControlCounter === 15.U || regNewData === 1.U, 0.U, outControlCounter + 1.U)
+
+  when (outControlCounter === 0.U) {
+    io.red := regMuxRed0
+    io.green := regMuxRed0
+    io.blue := regMuxRed0
+  }
+  .elsewhen( outControlCounter === 1.U) {
+    io.red := regMuxRed1
+    io.green := regMuxRed1
+    io.blue := regMuxRed1
+  }
+  .elsewhen( outControlCounter === 2.U) {
+    io.red := regMuxRed2
+    io.green := regMuxRed2
+    io.blue := regMuxRed2
+  }
+  .elsewhen( outControlCounter === 3.U) {
+    io.red := regMuxRed3
+    io.green := regMuxRed3
+    io.blue := regMuxRed3
+  }
+  .elsewhen( outControlCounter === 4.U) {
+    io.red := regMuxRed4
+    io.green := regMuxRed4
+    io.blue := regMuxRed4
+  }
+  .elsewhen( outControlCounter === 5.U) {
+    io.red := regMuxRed5
+    io.green := regMuxRed5
+    io.blue := regMuxRed5
+  }
+  .elsewhen( outControlCounter === 6.U) {
+    io.red := regMuxRed6
+    io.green := regMuxRed6
+    io.blue := regMuxRed6
+  }
+  .elsewhen( outControlCounter === 7.U) {
+    io.red := regMuxRed7
+    io.green := regMuxRed7
+    io.blue := regMuxRed7
+  }
+  .elsewhen( outControlCounter === 8.U) {
+    io.red := regMuxRed8
+    io.green := regMuxRed8
+    io.blue := regMuxRed8
+  }
+  .elsewhen( outControlCounter === 9.U) {
+    io.red := regMuxRed9
+    io.green := regMuxRed9
+    io.blue := regMuxRed9
+  }
+  .elsewhen( outControlCounter === 10.U) {
+    io.red := regMuxRed10
+    io.green := regMuxRed10
+    io.blue := regMuxRed10
+  }
+  .elsewhen( outControlCounter === 11.U) {
+    io.red := regMuxRed11
+    io.green := regMuxRed11
+    io.blue := regMuxRed11
+  }
+  .elsewhen( outControlCounter === 12.U) {
+    io.red := regMuxRed12
+    io.green := regMuxRed12
+    io.blue := regMuxRed12
+  }
+  .elsewhen( outControlCounter === 13.U) {
+    io.red := regMuxRed13
+    io.green := regMuxRed13
+    io.blue := regMuxRed13
+  }
+  .elsewhen( outControlCounter === 14.U) {
+    io.red := regMuxRed14
+    io.green := regMuxRed14
+    io.blue := regMuxRed14
+  }
+  .otherwise {
+    io.red := regMuxRed15
+    io.green := regMuxRed15
+    io.blue := regMuxRed15
+  }
+
+  io.vSync := 0.U
+}
---
+++ src/test/scala/mm2hdmi/MM2hdmiUnitTest.scala
@@ -0,0 +1,155 @@
+// See README.md for license details.
+
+package mm2hdmi
+
+import java.io.File
+
+import chisel3.iotesters
+import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
+
+class MM2hdmiUnitTester(c: MM2hdmi) extends PeekPokeTester(c) {
+  /**
+    * compute the mm2hdmi and the number of steps it should take to do it
+    *
+    * @param a positive integer
+    * @param b positive integer
+    * @return the GCD of a and b
+    */
+  def computeRGB(d: Int): (Int, Int, Int, Int, Int) = {
+    var data = d
+    var R = 0
+    var G = 0
+    var B = 0
+    var vSync = 0
+    var hSync = 0
+
+    if ( d == 0 ) {
+        R = 0
+        G = 0
+        B = 0
+      }
+      else {
+        R = 255
+        G = 255
+        B = 255
+      }
+    
+    (R, G, B, vSync, hSync)
+  }
+
+  def toBinary(n: Int): String = n match {
+    case 0|1 => s"$n"
+    case _   => s"${toBinary(n/2)}${n%2}"
+  }
+
+  def singleOutTheData(data: Int): String = {
+
+    var intBinary = toBinary(data).toInt
+    var dBinary = "%016d".format(intBinary)
+    dBinary.reverse
+  }
+
+  private val mm2hdmi = c
+
+
+    for (i <- 0 to 100) {
+      poke(mm2hdmi.io.data, i)
+      poke(mm2hdmi.io.newData, 1)
+      step(1)
+      poke(mm2hdmi.io.newData, 0)
+      //poke(mm2hdmi.io.address, j)
+
+      var dBits = singleOutTheData(i)
+      printf("i je %d\n", i)
+      printf("i string je %s\n", dBits)
+      for ( j <- dBits) {
+        step(1)
+        val (expectedR, expectedG, expectedB, expectedVSync, expectedHSync) = computeRGB((j.toInt)-48)
+        printf("dbits od j je %d\n", (j.toInt)-48)
+        expect(mm2hdmi.io.red, expectedR)
+        expect(mm2hdmi.io.green, expectedG)
+        expect(mm2hdmi.io.blue, expectedB)
+      }
+      
+    }
+
+}
+
+/**
+  * This is a trivial example of how to run this Specification
+  * From within sbt use:
+  * {{{
+  * testOnly mm2hdmi.MM2hdmiTester
+  * }}}
+  * From a terminal shell use:
+  * {{{
+  * sbt 'testOnly mm2hdmi.MM2hdmiTester'
+  * }}}
+  */
+class MM2hdmiTester extends ChiselFlatSpec {
+  private val backendNames = if(firrtl.FileUtils.isCommandAvailable(Seq("verilator", "--version"))) {
+    Array("firrtl", "verilator")
+  }
+  else {
+    Array("firrtl")
+  }
+  for ( backendName <- backendNames ) {
+    "MM2hdmi" should s"calculate proper greatest common denominator (with $backendName)" in {
+      Driver(() => new MM2hdmi, backendName) {
+        c => new MM2hdmiUnitTester(c)
+      } should be (true)
+    }
+  }
+
+  "Basic test using Driver.execute" should "be used as an alternative way to run specification" in {
+    iotesters.Driver.execute(Array(), () => new MM2hdmi) {
+      c => new MM2hdmiUnitTester(c)
+    } should be (true)
+  }
+
+  if(backendNames.contains("verilator")) {
+    "using --backend-name verilator" should "be an alternative way to run using verilator" in {
+      iotesters.Driver.execute(Array("--backend-name", "verilator"), () => new MM2hdmi) {
+        c => new MM2hdmiUnitTester(c)
+      } should be(true)
+    }
+  }
+
+  "running with --is-verbose" should "show more about what's going on in your tester" in {
+    iotesters.Driver.execute(Array("--is-verbose"), () => new MM2hdmi) {
+      c => new MM2hdmiUnitTester(c)
+    } should be(true)
+  }
+
+  /**
+    * By default verilator backend produces vcd file, and firrtl and treadle backends do not.
+    * Following examples show you how to turn on vcd for firrtl and treadle and how to turn it off for verilator
+    */
+
+  "running with --generate-vcd-output on" should "create a vcd file from your test" in {
+    iotesters.Driver.execute(
+      Array("--generate-vcd-output", "on", "--target-dir", "test_run_dir/make_a_vcd", "--top-name", "make_a_vcd"),
+      () => new MM2hdmi
+    ) {
+
+      c => new MM2hdmiUnitTester(c)
+    } should be(true)
+
+    new File("test_run_dir/make_a_vcd/MM2hdmi.vcd").exists should be (true)
+  }
+
+  "running with --generate-vcd-output off" should "not create a vcd file from your test" in {
+    iotesters.Driver.execute(
+      Array("--generate-vcd-output", "off", "--target-dir", "test_run_dir/make_no_vcd", "--top-name", "make_no_vcd",
+      "--backend-name", "verilator"),
+      () => new MM2hdmi
+    ) {
+
+      c => new MM2hdmiUnitTester(c)
+    } should be(true)
+
+    new File("test_run_dir/make_no_vcd/make_a_vcd.vcd").exists should be (false)
+
+  }
+
+}
